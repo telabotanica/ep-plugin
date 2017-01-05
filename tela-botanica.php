25f3ea3ef818a6b6a378367d3194eeac47c48542
Mathias Chouet <mathias@tela-botanica.org>
2017-01-05 15:23:50 +0100

    Catégories de projets réimplémentées avec Buddypress

--- tela-botanica.php
+++ tela-botanica.php
@@ -59,7 +59,99 @@ function initialisation_bp()
 // amorçage du plugin lors de l'amorçage de BuddyPress
 add_action( 'bp_include', 'initialisation_bp' );
 add_action( 'bp_include', 'description_complete' );
+add_action( 'bp_groups_register_group_types', 'tb_ajout_categories_projets' );
 //add_action( 'bp_include', 'categorie' );
+
+/**
+ * Ajoute à BP les catégories de projets Tela Botanica
+ * 
+ * @TODO déplacer les descriptions dans config.json ?
+ */
+function tb_ajout_categories_projets()
+{
+	bp_groups_register_group_type('echanges', array(
+		'labels' => array(
+			'name' => 'Échanges',
+			'singular_name' => 'Échanges'
+		),
+		'has_directory' => 'Échanges',
+		'show_in_create_screen' => true,
+		'show_in_list' => true,
+		'description' => "Discussion",
+		'create_screen_checked' => true
+	));
+
+	bp_groups_register_group_type('botanique-locale', array(
+		'labels' => array(
+			'name' => 'Botanique locale',
+			'singular_name' => 'Botanique locale'
+		),
+		'has_directory' => 'botanique-locale',
+		'show_in_create_screen' => true,
+		'show_in_list' => true,
+		'description' => "Botanique en territoire : rencontres, sorties de terrain...",
+		'create_screen_checked' => false
+	));
+
+	bp_groups_register_group_type('outils-informatiques', array(
+		'labels' => array(
+			'name' => 'Outils informatiques',
+			'singular_name' => 'Outils informatiques'
+		),
+		'has_directory' => 'Outils informatiques',
+		'show_in_create_screen' => true,
+		'show_in_list' => true,
+		'description' => "Projets présentant les outils et recevant les remarques (gné?)",
+		'create_screen_checked' => false
+	));
+
+	bp_groups_register_group_type('organisation', array(
+		'labels' => array(
+			'name' => 'Organisation',
+			'singular_name' => 'Organisation'
+		),
+		'has_directory' => 'organisation',
+		'show_in_create_screen' => true,
+		'show_in_list' => true,
+		'description' => "Co-organisation de programmes, comités de pilotage...",
+		'create_screen_checked' => false
+	));
+
+	bp_groups_register_group_type('contribution', array(
+		'labels' => array(
+			'name' => 'Contribution',
+			'singular_name' => 'Contribution'
+		),
+		'has_directory' => 'contribution',
+		'show_in_create_screen' => true,
+		'show_in_list' => true,
+		'description' => "Saisie d'observations botaniques",
+		'create_screen_checked' => false
+	));
+
+	bp_groups_register_group_type('construction', array(
+		'labels' => array(
+			'name' => 'Construction',
+			'singular_name' => 'Construction'
+		),
+		'has_directory' => 'construction',
+		'show_in_create_screen' => true,
+		'show_in_list' => true,
+		'description' => "Création de banque de données",
+		'create_screen_checked' => false
+	));
+
+	bp_groups_register_group_type('tela-botanica', array(
+		'labels' => array(
+			'name' => 'Tela Botanica',
+			'singular_name' => 'Tela Botanica'
+		),
+		'has_directory' => 'tela-botanica',
+		'show_in_create_screen' => false,
+		'show_in_list' => true,
+		'description' => "Projets officiels de Tela Botanica"
+	));
+}
 
 // Charge les hooks de synchronisation des données de wordpress vers les autres outils
 require( dirname( __FILE__ ) . '/hooks/hooks.php' );
