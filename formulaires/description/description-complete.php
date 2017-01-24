ff2dc7947fca016869e886ffec9fe4c35fc82c93
Mathias Chouet <mathias@tela-botanica.org>
2017-01-24 15:48:09 +0100

    Description complète : éditeur WYSIWYG WP

--- formulaires/description/description-complete.php
+++ formulaires/description/description-complete.php
@@ -1,19 +1,31 @@
 <?php
 
 function description_complete() {
-	
-	// Retourne l'id du projet courant et la valeur meta_key de ce projet
-	function get_value($meta_key='') {
-		return groups_get_groupmeta(bp_get_group_id(), $meta_key);
+
+	// Retourne la valeur meta_key pour le projet courant
+	function get_value($meta_key) {
+		$id = bp_get_group_id();
+		if ($id != null) {
+			return groups_get_groupmeta($id, $meta_key);
+		} else {
+			return '';
+		}
 	}
-	
+
 	// Formulaire pour la description complète du projet
 	function description_complete_formulaire() {
 		global $bp, $wpdb;
 		?>
 		<p class="editfield">
-			<label for="description-complete">Description complète</label>
-			<textarea style="width: 100%; max-width: 100%; min-height: 200px; resize: vertical;" id="description-complete" name="description-complete"><?php echo get_value('description-complete'); ?></textarea>
+			<label for="description-complete"><?php _e("Description complète", 'telabotanica') ?></label>
+			<span class="description"><?php _e("Sera affichée sur la page d'accueil du projet", 'telabotanica') ?></span>
+			<?php
+			wp_editor(get_value('description-complete'), 'description-complete', array(
+				'media_buttons' => false,
+				//'teeny' => true,
+				'quicktags' => false
+			));
+			?>
 		</p>
 		<?php
 	}
@@ -35,11 +47,4 @@ function description_complete() {
 	add_filter( 'groups_custom_group_fields_editable', 'description_complete_formulaire' );
 	add_action( 'groups_group_details_edited', 'description_complete_enregistrement' );
 	add_action( 'groups_created_group',  'description_complete_enregistrement' );
-
-	// Affichage de la description complète surla page d'accueil
-	function affichage_description() {
-		echo '<div id="description-complete">' . nl2br(get_value('description-complete')) . '</div>';
-	}
-	// hook du template "groups/single/front"
-	add_action('bp_group_front_content' , 'affichage_description');
 }
