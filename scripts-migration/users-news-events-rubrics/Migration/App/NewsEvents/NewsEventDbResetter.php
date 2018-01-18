a17fac183fb75119660f60236153ce60f0ec67b2
whatever22 <s.renaudier@gmail.com>
2018-01-18 13:50:06 +0100

    Fixed #849. (#31)
    * Fixed #849.
    
    * Added README.md + fixed a small "coquille" in an exception message (CoversSync.php).
    
    * Edited README.md.
    
    * Edited README.md to remove glitches and add clarifications according to @kstefaninis judicious comments
    
    * Fixed broken alsacian mode. Got fotami... Afais cassé le mote elssassien...

--- scripts-migration/users-news-events-rubrics/Migration/App/NewsEvents/NewsEventDbResetter.php
+++ scripts-migration/users-news-events-rubrics/Migration/App/NewsEvents/NewsEventDbResetter.php
@@ -8,7 +8,7 @@ use \Exception;
 
 /**
  * Deletes all posts (along with metas and slug histories)
- * corresponding to previously imported news and events. 
+ * corresponding to previously imported news and events.
  */
 class NewsEventDbResetter extends QueryDbResetter {
 
@@ -17,6 +17,7 @@ class NewsEventDbResetter extends QueryDbResetter {
         parent::setQueries([
           'DELETE FROM posts p1 JOIN posts p2 ON (p1.post_parent = p2.ID AND p2.post_type = "post")',
           'DELETE FROM slug_history JOIN post ON (post.ID = slug_history.post_id AND post.post_type = "post")',
+          'DELETE FROM icl_translations JOIN post ON (post.ID = icl_translations.element_id AND post.post_type = "post")',
           'DELETE FROM postmeta JOIN posts ON (posts.ID = postmeta.post_id AND posts.post_type = "post")'
         ]);
     }
