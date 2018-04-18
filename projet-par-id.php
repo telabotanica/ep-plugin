cba43caf837f5d704998e0e2c0c3a0e40448a788
Mathias Chouet <mathias@tela-botanica.org>
2018-04-18 15:48:42 +0200

    Travail en cours

--- projet-par-id.php
+++ projet-par-id.php
@@ -8,25 +8,30 @@ require_once __DIR__ . "/../../../wp-load.php";
 $id = false;
 // si le paramètre "id" est fourni
 if (! empty($_GET['id']) && is_numeric($_GET['id'])) {
-	$id = $_GET['id'];
+    $id = $_GET['id'];
 } else { // sinon on fouille dans l'URL
-	$parsedUrl = wp_parse_url("//".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']);
-	if (! empty($parsedUrl['path'])) {
-		$urlParts = explode('/', $parsedUrl['path']);
-		if (count($urlParts > 0)) {
-			$lastPart = array_pop($urlParts);
-			while ($lastPart == "") {
-				$lastPart = array_pop($urlParts);
-			}
-			if (is_numeric($lastPart)) {
-				$id = $lastPart;
-			}
-		}
-	}
+    $parsedUrl = wp_parse_url("//".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']);
+    if (! empty($parsedUrl['path'])) {
+        $urlParts = explode('/', $parsedUrl['path']);
+        if (count($urlParts > 0)) {
+            $lastPart = array_pop($urlParts);
+            while ($lastPart == "") {
+                $lastPart = array_pop($urlParts);
+            }   
+            while (! is_numeric($lastPart)){
+                $stringPart .= "/" . $lastPart;
+                $lastPart = array_pop($urlParts);
+            }   
+            if (is_numeric($lastPart)) {
+                $id = $lastPart;
+            }   
+        }   
+    }   
 }
 
 // recherche de l'URL du groupe par son ID
 // si le groupe n'existe pas, le permalink pointera sur la liste des groupes (pratique)
 $group = groups_get_group(array('group_id' => $id));
 $permalink = bp_get_group_permalink($group);
+$permalink = $permalink . $stringPart;
 wp_redirect($permalink);
