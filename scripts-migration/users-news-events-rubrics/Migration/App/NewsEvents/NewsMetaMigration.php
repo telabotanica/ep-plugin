4eb5ef8821f74d763d54641cdcba3bbf410d5235
kstefanini <stefanini.k@gmail.com>
2018-06-21 18:05:31 +0200

    Add iframe component to news import

--- scripts-migration/users-news-events-rubrics/Migration/App/NewsEvents/NewsMetaMigration.php
+++ scripts-migration/users-news-events-rubrics/Migration/App/NewsEvents/NewsMetaMigration.php
@@ -46,6 +46,44 @@ class NewsMetaMigration extends BaseMigration {
           throw new MigrationException($e, $req, basename(__FILE__) . ':' . __FUNCTION__);
         }
       }
+
+      // search for iframe and there height
+      if (preg_match_all("@<iframe src=\"http(.*)\" (?:.*height: (\d+)px.*)?<\/iframe>@", $article['post_content'], $matches)) {
+        $values = '';
+        $components = [];
+        foreach($matches[0] as $id => $match) {
+          $components[] = 'embed';
+          $values .= "
+            ({$article['ID']}, 'components_{$id}_method',  'iframe'),
+            ({$article['ID']}, '_components_{$id}_method', 'field_581795fe075cc_field_5817976d9ed35'),
+            ({$article['ID']}, 'components_{$id}_iframe',  'https{$matches[1][$id]}'),
+            ({$article['ID']}, '_components_{$id}_iframe', 'field_581795fe075cc_field_5817972c0cb54'),
+            ({$article['ID']}, 'components_{$id}_height',  '{$matches[2][$id]}'),
+            ({$article['ID']}, '_components_{$id}_height', 'field_581795fe075cc_field_582c5263899c7'),
+            ({$article['ID']}, 'components_{$id}_description',  ''),
+            ({$article['ID']}, '_components_{$id}_description', 'field_581795fe075cc_field_58179c698ffe2'),
+          ";
+        }
+
+        $components = serialize($components);
+        $values .= "
+          ({$article['ID']}, 'components',  '$components'),
+          ({$article['ID']}, '_components', 'field_58177612a0f3b')
+        ";
+
+        $req = "INSERT INTO {$this->wpTablePrefix}postmeta (`post_id`, `meta_key`, `meta_value`) VALUES $values ";
+
+        try {
+          $this->wpDbConnection->exec($req);
+          // // Verbose
+          // echo $compteurSucces . PHP_EOL;
+          $compteurSucces++;
+        } catch(Exception $e) {
+          echo "-- ECHEC " . basename(__FILE__) . ':' . __FUNCTION__ . " REQUÊTE: [$req]" . PHP_EOL;
+
+          throw new MigrationException($e, $req, basename(__FILE__) . ':' . __FUNCTION__);
+        }
+      }
     }
 
     echo '-- ' . $compteurSucces . ' metas d\'actualités migrées. ' . PHP_EOL;
